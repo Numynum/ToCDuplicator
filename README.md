3c5285e0e3a36585fe3ac769135b5309fe0ed560
Mark W <numedain@gmail.com>
2021-05-30 15:18:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# duplicateToc.sh
+
+__duplicateToc.sh__ will create .toc files specific to each wow version.
+See https://github.com/Stanzilla/WoWUIBugs/issues/68#issuecomment-830351390 for more information
+
+The best way to use this, is as part of your addon builds, between checkout and using a packager (e.g. bigwigs packager)
+
+Example usage:
+
+    steps:
+      - uses: actions/checkout@v2
+
+      - name: Replace toc-versions
+        uses: Numynum/ToCVersions@master
+
+      - name: Create BCC and Classic ToC files
+        uses: Numynum/ToCDuplicator@master
+        with:
+            files: path/to/module/*.toc *.toc
+            retail: true
+            bcc: false
+            classic: true
+
+      - name: Create Retail Package
+        uses: BigWigsMods/packager@master
+
+An example .toc file could be:
+
+    ## Interface: 1234 # if hardcoded
+    ## Interface-Retail: @toc-version-retail@ # if using ToCVersions action
+    ## Interface-Classic: @toc-version-classic@
+    ## Interface-BCC: @toc-version-bcc@
+    ## Title: MyEpicAddon
+    ## Notes: Making addons great again!
+    ## Author: EpicAddonWriter2000
+    ## Version: @project-version@
+
+After each file is created, the `## Interface-XX` line for that file will be removed from the source .toc file.
+
+## Version arguments
+
+By default, this script will attempt to create a file for each version (retail|bcc|classic).
+This can be explicitly disabled by passing `0` or `false`, as in the example above.
+
+Leaving out these arguments is the same as passing `true`.
+
+## Files argument
+
+By default, this script will parse all .toc files in your root directory.
+If this isn't enough, you can instead pass a list of files to replace.
+Version specific files are never used as a source, but WILL be overwritten if configured.
+
+      - name: Replace toc-versions
+        uses: Numynum/ToCVersions@master
+        with:
+          files: path/to/module/*.toc *.toc
---
+++ duplicateToc.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+function duplicate() {
+  local file="$1"
+
+  if [ -n "${retail}" ]; then
+    if ! grep -q "## Interface-Retail:" "$file"; then
+      echo "No ## Interface-Retail found in $file. Skipping."
+    else
+      local target="${file/.toc/-Mainline.toc}"
+      echo "Creating file $target"
+      cp "$file" "$target"
+      sed -ri "/^## Interface:.*\$/d" "$target"
+      sed -ri "/^## Interface-BCC:.*\$/d" "$target"
+      sed -ri "/^## Interface-Classic:.*\$/d" "$target"
+
+		  sed -ri "s/^(## Interface)-Retail(:.*)\$/\1\2/" "$target"
+
+      sed -ri "/^## Interface-Retail:.*\$/d" "$file"
+    fi
+  fi
+
+  if [ -n "${bcc}" ]; then
+    if ! grep -q "## Interface-BCC:" "$file"; then
+      echo "No ## Interface-BCC found in $file. Skipping."
+    else
+      local target="${file/.toc/-BCC.toc}"
+      echo "Creating file $target"
+      cp "$file" "$target"
+      sed -ri "/^## Interface:.*\$/d" "$target"
+      sed -ri "/^## Interface-Retail:.*\$/d" "$target"
+      sed -ri "/^## Interface-Classic:.*\$/d" "$target"
+
+		  sed -ri "s/^(## Interface)-BCC(:.*)\$/\1\2/" "$target"
+
+      sed -ri "/^## Interface-BCC:.*\$/d" "$file"
+    fi
+  fi
+
+  if [ -n "${classic}" ]; then
+    if ! grep -q "## Interface-Classic:" "$file"; then
+      echo "No ## Interface-Classic found in $file. Skipping."
+    else
+      local target="${file/.toc/-Classic.toc}"
+      echo "Creating file $target"
+      cp "$file" "$target"
+      sed -ri "/^## Interface:.*\$/d" "$target"
+      sed -ri "/^## Interface-Retail:.*\$/d" "$target"
+      sed -ri "/^## Interface-BCC:.*\$/d" "$target"
+
+		  sed -ri "s/^(## Interface)-Classic(:.*)\$/\1\2/" "$target"
+
+      sed -ri "/^## Interface-Classic:.*\$/d" "$file"
+    fi
+  fi
+}
+
+if [ -n "${retail}" ]; then
+  if [[ ("$retail" == "false") || ("$retail" == "0") ]]; then
+    retail=
+  fi
+else
+    retail=true
+fi
+
+if [ -n "${bcc}" ]; then
+  if [[ ("$bcc" == "false") || ("$bcc" == "0") ]]; then
+    bcc=
+  fi
+else
+    bcc=true
+fi
+
+if [ -n "${classic}" ]; then
+  if [[ ("$classic" == "false") || ("$classic" == "0") ]]; then
+    classic=
+  fi
+else
+    classic=true
+fi
+
+if [ $# -eq 0 ]; then
+  while read -r file; do
+    duplicate "$file"
+  done < <(find . -maxdepth 1 -name '*.toc' ! -name '*-Mainline.toc' ! -name '*-Classic.toc' ! -name '*-BCC.toc')
+else
+  for var in "$@"
+  do
+    if [[ "$var" != *"-Mainline.toc"* &&  "$var" != *"-Classic.toc"* &&  "$var" != *"-BCC.toc"* ]]; then
+      duplicate "$var"
+    fi
+  done
+fi
